be2287180bb965f417b1045d102f18c2c95f3be1
furious.io <furious@example.com>
2024-03-10 16:04:15 +0530

    changes

---
+++ src/app/aboutus/page.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function dashboard() {
+    return (
+        <div className="flex justify-center h-[700] bg-white text-black">
+            <h1>this is about us page!!!</h1>
+        </div>
+    )
+}
